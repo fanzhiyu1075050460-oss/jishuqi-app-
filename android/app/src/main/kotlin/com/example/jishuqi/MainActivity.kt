137c6c1bc5611fe27193cb85abd30ff61990fa99
fanzhiyu1075050460-oss <fanzhiyu1075050460@gmail.com>
2026-09-10 07:58:20 +0800

    Create MainActivity.kt

---
+++ android/app/src/main/kotlin/com/example/jishuqi/MainActivity.kt
@@ -0,0 +1,5 @@
+package包 com.example.jishuqi
+
+import io.flutter.embedding.android.FlutterActivity导入io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()类 MainActivity：FlutterActivity()类 MainActivity：FlutterActivity()类 MainActivity：FlutterActivity()
