009c1ee11a97111719178b0a8ff83ad16511c428
fanzhiyu1075050460-oss <fanzhiyu1075050460@gmail.com>
2026-09-10 13:12:58 +0800

    Delete  android directory

---  android/app/src/main/kotlin/com/example/jishuqi/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package包 com.example.jishuqi
-
-import io.flutter.embedding.android.FlutterActivity导入io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()类 MainActivity：FlutterActivity()类 MainActivity：FlutterActivity()类 MainActivity：FlutterActivity()
